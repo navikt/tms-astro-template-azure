be0f2f713763a4a06852c5321f1b73b9b8105b5c
Nima Hakimi <nima.hakimi2@nav.no>
2025-02-11 15:20:38 +0100

    Setup hsuky pre-commit and prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
+  overrides: [{ files: "*.astro", options: { parser: "astro" } }],
+};
--- src/middleware/urls.ts
+++ src/middleware/urls.ts
@@ -1,9 +1,9 @@
-import { getEnvironment } from "@src/utils/server/urls.ts";
+import { getEnvironment } from '@src/utils/server/urls.ts';
 
 const REDIRECT_URI = {
-  local: "http://localhost:4321/tms-astro-template",
-  development: "https://www.ansatt.dev.nav.no/tms-astro-template",
-  production: "https://www.nav.no/minside/tms-astro-template",
+  local: 'http://localhost:4321/tms-astro-template',
+  development: 'https://www.ansatt.dev.nav.no/tms-astro-template',
+  production: 'https://www.nav.no/minside/tms-astro-template',
 };
 
 export const redirectUri = REDIRECT_URI[getEnvironment()];
