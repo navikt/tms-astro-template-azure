76ecac43ba09d0dcd023695fe13be9d75cd51357
Nima Hakimi <nima.hakimi2@nav.no>
2025-02-11 15:30:26 +0100

    Adjust redirect url

--- src/middleware/urls.ts
+++ src/middleware/urls.ts
@@ -1,10 +1,10 @@
-import { getEnvironment } from '@src/utils/server/urls.ts';
+import { getEnvironment } from "@src/utils/server/urls.ts";
 
 const REDIRECT_URI = {
-  local: 'http://localhost:4321/tms-astro-template',
-  development: 'https://www.ansatt.dev.nav.no/tms-astro-template',
-  production: 'https://www.nav.no/minside/tms-astro-template',
+  local: "http://localhost:4321/",
+  development: "https://tms-astro-template.ansatt.dev.nav.no/",
+  production: "https://tms-astro-template.ansatt.nav.no/minside/",
 };
 
 export const redirectUri = REDIRECT_URI[getEnvironment()];
-export const loginUrl = `/tms-astro-template/oauth2/login?redirect=${redirectUri}`;
+export const loginUrl = `/oauth2/login?redirect=${redirectUri}`;
