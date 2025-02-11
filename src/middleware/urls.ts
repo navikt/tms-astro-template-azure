a2238ed27156e791e9459357bd834d6feed66b04
Nima Hakimi <nima.hakimi2@nav.no>
2025-02-11 15:33:44 +0100

    Correct production redirect url

--- src/middleware/urls.ts
+++ src/middleware/urls.ts
@@ -1,9 +1,9 @@
-import { getEnvironment } from '@src/utils/server/urls.ts';
+import { getEnvironment } from "@src/utils/server/urls.ts";
 
 const REDIRECT_URI = {
-  local: 'http://localhost:4321/',
-  development: 'https://tms-astro-template.ansatt.dev.nav.no/',
-  production: 'https://tms-astro-template.ansatt.nav.no/minside/',
+  local: "http://localhost:4321/",
+  development: "https://tms-astro-template.ansatt.dev.nav.no/",
+  production: "https://tms-astro-template.ansatt.nav.no/",
 };
 
 export const redirectUri = REDIRECT_URI[getEnvironment()];
